905313f75a0e3a983e1168abec98be1ca409f113
Asus Zenbook <anapiiaevsandary@gmail.com>
2024-03-19 00:05:02 +0300

    I change my code

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("write your age");
-        int age= scanner.nextInt();
-        System.out.println("your age is : "+age);
+        System.out.println("Hello world");
     }
 }
