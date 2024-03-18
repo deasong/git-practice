5536ac2cacaf8a333cc02272eb3342de0c2d09fd
Asus Zenbook <anapiiaevsandary@gmail.com>
2024-03-18 23:47:23 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("write your age");
+        int age= scanner.nextInt();
+        System.out.println("your age is : "+age);
+    }
+}
